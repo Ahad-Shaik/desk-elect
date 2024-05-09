0dd955eed249a516461e0bd06f6203afa9c70cf9
Ahad-Shaik <ahadpersonal69@gmail.com>
2024-05-10 00:00:07 +0530

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'electron-desktop',
+  webDir: 'www'
+};
+
+export default config;
